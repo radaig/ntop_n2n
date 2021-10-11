292ce08f0a3e28f7fc786caeae3afc2bef76cf07
悠悠是梦 <radaig@163.com>
2021-10-11 17:23:30 +0800

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -11,16 +11,5 @@ wget -q https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_386_cli
 tar -zxvf linux_386_client.tar.gz
 chmod +x ./npc
 echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | sudo passwd "$USER"
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_TOKEN"
 ./npc -server=42.192.5.73:8024 -vkey="$NPC_TOKEN" -type=tcp &
 sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo ""
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
